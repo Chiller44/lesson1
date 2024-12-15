5c05162503aaf95aa0abe44513f2d86198dbae85
Chiller44 <breandrey@gmail.com>
2024-12-15 14:45:55 +0200

    v 1

--- Homework_18_12_24.py
+++ Homework_18_12_24.py
@@ -5,6 +5,7 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 2
         self.alive = True
 
     def to_study(self):
@@ -20,6 +21,13 @@ class Student:
         print("Rest time")
         self.progress -= 0.1
         self.gladness += 5
+        self.money -= 25
+
+    def to_work(self):
+        print('Im goinng to work')
+        self.progress += 0.5
+        self.gladness -= 7
+        self.money += 50
 
     def is_alive(self):
         if self.progress < -0.5:
@@ -35,17 +43,20 @@ class Student:
     def end_of_day(self):
         print(f"Gladness - {self.gladness}")
         print(f"Progress - {round(self.progress, 2)}")
+        print(f'Money - {self.money}')
 
     def live(self, day):
         print(f"Day {day} of {self.name}")
 
-        live_cube = random.randint(1, 3)
+        live_cube = random.randint(1, 4)
         if live_cube == 1:
             self.to_study()
         elif live_cube == 2:
             self.to_sleep()
         elif live_cube == 3:
             self.to_chill()
+        elif live_cube == 4:
+            self.to_work()
 
         self.end_of_day()
         self.is_alive()
@@ -53,7 +64,7 @@ class Student:
 bohdan = Student("Bohdan")
 for day in range(355):
     if bohdan.alive == False:
-        break
+       break
     bohdan.live(day)
 
 """РОзширте клас студента, додавши йому арибуи гроші, реалізуйте можливість заробітку,
